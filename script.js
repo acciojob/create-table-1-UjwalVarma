eecc5d1e5885019ac0407f1e25e3a8a6a0416637
Ujwal Varma <120555973+UjwalVarma@users.noreply.github.com>
2024-02-23 22:16:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 function insert_Row() {
     //Write your code here
+	// script.js
+
+function insert_Row() {
+    // Get the reference to the table
+    var table = document.getElementById("sampleTable");
+
+    // Insert a new row at the top
+    var newRow = table.insertRow(0);
+
+    // Insert cells into the new row
+    var cell1 = newRow.insertCell(0);
+    var cell2 = newRow.insertCell(1);
+
+    // Set the values for the new cells
+    cell1.innerHTML = "New Cell1";
+    cell2.innerHTML = "New Cell2";
+}
+
   
   
 }
