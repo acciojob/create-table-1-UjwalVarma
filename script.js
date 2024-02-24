54771010785598385b6d32dd49f0d72500433ee7
Ujwal Varma <120555973+UjwalVarma@users.noreply.github.com>
2024-02-24 11:57:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,17 @@
 function insert_Row() {
     //Write your code here
-	// script.js
+    let table = document.getElementById('sampleTable');
+    let btn = document.querySelector('input'); 
 
-function insert_Row() {
-    // Get the reference to the table
-    var table = document.getElementById("sampleTable");
 
-    // Insert a new row at the top
-    var newRow = table.insertRow(0);
+    btn.addEventListener('click', addRow) 
 
-    // Insert cells into the new row
-    var cell1 = newRow.insertCell(0);
-    var cell2 = newRow.insertCell(1);
+    function addRow() {
+        console.log("I am executed")
+        let row = table.insertRow();
+        row.insertCell(0).innerHTML = "New Cell1";
+        row.insertCell(1).innerHTML = "New Cell2";
+    }
 
-    // Set the values for the new cells
-    cell1.innerHTML = "New Cell1";
-    cell2.innerHTML = "New Cell2";
-}
 
-  
-  
 }
